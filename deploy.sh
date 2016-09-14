3f30125e6247b6da68c96f0647eb96c2aaf27620
Alex Boyce <acb222@smeal.psu.edu>
2016-09-14 13:25:11 -0400

    Force gh-pages branch

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 set -o errexit -o nounset
 
 # Get into the dist
-
 cd dist
 
 # Init
@@ -17,6 +16,7 @@ git config user.name "Travis CI"
 git remote add upstream "https://$GITHUB_TOKEN@github.com/open-curriculum/oerschema.git"
 
 # Add, Commit and Push
+git checkout -b gh-pages
 git add --all
 git commit -m "Auto push from Travis"
 git push -u upstream gh-pages --force
