ecac0e79bd9a592c2a3ddccace3d0d6b59444bad
Alex Boyce <acb222@smeal.psu.edu>
2016-09-14 12:34:42 -0400

    Prepare the dist before updating it

---
+++ prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+# config
+git config user.email "nobody@nobody.org"
+git config user.name "Travis CI"
+
+# set the remote
+
+git remote add upstream "https://$GITHUB_TOKEN@github.com/open-curriculum/oerschema.git"
+
+# update the dist folder
+git subtree pull --prefix dist upstream gh-pages
