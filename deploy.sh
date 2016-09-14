17e5c00e1763004d06b6850c1cc3e023fc96f058
Alex Boyce <acb222@smeal.psu.edu>
2016-09-14 12:51:51 -0400

    Update dist folder before making changes - bulldozing

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,12 @@ set -o errexit -o nounset
 git config user.email "nobody@nobody.org"
 git config user.name "Travis CI"
 
+# set the remote
+
+git remote add upstream "https://$GITHUB_TOKEN@github.com/open-curriculum/oerschema.git"
+
 # Add, Commit and Push
 
 git add dist
 git commit -m "Auto push from Travis"
-git subtree push -m "Pushing updated" --prefix dist upstream gh-pages
+git push -m "Pushing updated" upstream `git subtree split --prefix dist master`:gh-pages --force
