b66bfc97583fbeeec9ebe741a6efffee28756dcd
Travis CI <nobody@nobody.org>
2016-12-14 10:12:57 -0500

    Fix issue with Travis Deployment

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,11 @@ git config --global user.name "Travis CI"
 git clone "https://$GITHUB_TOKEN@github.com/open-curriculum/oerschema.git" -b gh-pages _dist
 
 # Run Gulp
-gulp
-cd _dist/
-ls | grep -v .git | xargs rm -R #clean the directory to remove no longer used files
+gulp assets
+gulp buildSchema
+gulp template
+cd _dist
+#ls | grep -v .git . | xargs rm -R #clean the directory to remove no longer used files
 cp -R ../dist/* . # copy over the new, correct build
 ls -l
 
